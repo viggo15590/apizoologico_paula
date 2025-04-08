aa47247ad6a04d8436aa32183de61bc776e02155
miusuariogit <micorreo@correo.com>
2025-04-08 16:26:40 -0500

    clase 2

--- src/routes/animal.js
+++ src/routes/animal.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router(); //manejador de rutas de express
 const animalSchema = require("../models/animal");
+const verifyToken = require('./validate_token');
 //Nuevo animal
 router.post("/animals", (req, res) => {
     const animal = animalSchema(req.body);
@@ -46,6 +47,12 @@ router.delete("/animals/:id", (req, res) => {
         .catch((error) => {
             res.json({ message: error });
         });
+});
+//consultar todos los animales
+router.get("/animals", verifyToken, (req, res) => {
+    animalSchema.find()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error}));
 });
 
 module.exports = router;
